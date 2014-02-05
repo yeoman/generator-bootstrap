c077d2b1067b16b0141711835b2d768e62c589f1
Addy Osmani <addyosmani@gmail.com>
2014-02-05 22:20:15 +0000

    sass-bootstrap -> bootstrap-sass

--- app/index.js
+++ app/index.js
@@ -43,7 +43,7 @@ Generator.prototype.bootstrapFiles = function bootstrapFiles() {
   // map format -> package name
   var packages = {
     css: 'bootstrap.css',
-    sass: 'sass-bootstrap',
+    sass: 'bootstrap-sass',
     less: 'components-bootstrap',
     stylus: 'bootstrap-stylus'
   };
